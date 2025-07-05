5e72135794e7f5153569e09a25ed05dc05803618
chenxiangting <chenxiangting@huawei.com>
2025-07-05 15:16:56 +0800

    [BiShengIR] add interfaces

---
+++ bishengir/include/bishengir/Interfaces/AggregatedOpInterface.h
@@ -0,0 +1,20 @@
+//===- AggregatedOpInterface.h --------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef BISHENGIR_DIALECT_BISHENGIR_INTERFACES_AGGREGATEDOPINTERFACE_H
+#define BISHENGIR_DIALECT_BISHENGIR_INTERFACES_AGGREGATEDOPINTERFACE_H
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpDefinition.h"
+
+// Include the generated interface declarations.
+#include "bishengir/Interfaces/BiShengIREnums.h.inc"
+
+#include "bishengir/Interfaces/AggregatedOpInterface.h.inc"
+
+#endif // BISHENGIR_DIALECT_BISHENGIR_INTERFACES_AGGREGATEDOPINTERFACE_H
